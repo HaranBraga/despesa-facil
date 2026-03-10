a44878ceb7c62381db247ea510f1f4ef71c13581
Haran Braga <haran@braga>
2026-03-10 17:56:47 -0500

    fix(api): altera rotas do backend para ter prefixo /api e corrige proxy do NGINX para evitar 404 em prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,7 @@ export default defineConfig({
         proxy: {
             '/api': {
                 target: 'http://backend:3000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+                changeOrigin: true
             }
         }
     },
